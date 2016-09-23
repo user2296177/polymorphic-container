5aca536adc8eb082d75d2e46e866d6f5c379d400
user2296177 <boomshinesaigon@gmail.com>
2016-09-23 01:24:54 -0400

    Renamed function parameter names to be more consistent: dst is now nsrc where approriate

--- handle_base.h
+++ handle_base.h
@@ -17,24 +17,24 @@ namespace gut
 
 		virtual ~handle_base() = default;
 
-		handle_base( handle_base&& other ) noexcept;
-		handle_base& operator=( handle_base&& other ) noexcept;
+		handle_base(handle_base&& other) noexcept;
+		handle_base& operator=(handle_base&& other) noexcept;
 
-		handle_base( handle_base const& ) = delete;
-		handle_base& operator=( handle_base const& ) = delete;
+		handle_base(handle_base const&) = delete;
+		handle_base& operator=(handle_base const&) = delete;
 
 		virtual size_type align() const noexcept = 0;
 		virtual size_type size() const noexcept = 0;
 
 		virtual void destroy() = 0;
-		virtual void transfer( void* blk, void* dst ) = 0;
-		virtual void copy( void* blk, void* dst, polymorphic_handle& out_handle ) const = 0;
+		virtual void transfer(void* blk, void* nsrc) = 0;
+		virtual void copy(void* blk, void* dst, polymorphic_handle& out_handle) const = 0;
 
 		void* blk() const noexcept;
 		void* src() const noexcept;
 
 	protected:
-		handle_base( void* blk, void* src ) noexcept;
+		handle_base(void* blk, void* src) noexcept;
 
 		void* blk_;
 		void* src_;
@@ -43,16 +43,16 @@ namespace gut
 //////////////////////////////////////////////////////////////////////////////////
 // constructors
 //////////////////////////////////////////////////////////////////////////////////
-inline gut::handle_base::handle_base( handle_base&& other ) noexcept
+inline gut::handle_base::handle_base(handle_base&& other) noexcept
 	: blk_{ other.blk_ }
 	, src_{ other.src_ }
 {
 	other.src_ = nullptr;
 }
 
-inline gut::handle_base& gut::handle_base::operator=( handle_base&& other ) noexcept
+inline gut::handle_base& gut::handle_base::operator=(handle_base&& other) noexcept
 {
-	if ( this != &other )
+	if (this != &other)
 	{
 		blk_ = other.blk_;
 		src_ = other.src_;
@@ -61,7 +61,7 @@ inline gut::handle_base& gut::handle_base::operator=( handle_base&& other ) noex
 	return *this;
 }
 
-inline gut::handle_base::handle_base( void* blk, void* src ) noexcept
+inline gut::handle_base::handle_base(void* blk, void* src) noexcept
 	: blk_{ blk }
 	, src_{ src }
 {}
@@ -77,5 +77,4 @@ inline void* gut::handle_base::src() const noexcept
 {
 	return src_;
 }
-
 #endif // GUT_HANDLE_BASE_H
